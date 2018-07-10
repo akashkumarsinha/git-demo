0d9a7e5e107d3978d7101e74ff7897e083eb7668
unknown <akash051990@gmail.com>
2018-07-10 22:18:34 +0530

    added if condition to check the data

--- src/main/java/com/ericsson/app/git_demo/App.java
+++ src/main/java/com/ericsson/app/git_demo/App.java
@@ -7,6 +7,11 @@ package com.ericsson.app.git_demo;
 public class App {
 	public void addData(String data) {
 		System.out.println("data : " + data);
+		if(data.equals("Java Techie")) {
+			System.out.println("welcome");
+		}
+		else
+			System.out.println("BYE-BYE");
 	}
 
 	public static void main(String[] args) {
